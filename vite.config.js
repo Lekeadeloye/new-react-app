2808dedd1cc98e8485be38aad983f34ec1ccfd8c
Lekeadeloye <lekeadeloye@yahoo.com>
2023-10-11 17:53:30 +0100

    deleted dist folder from gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/new-react-app",
+  base: "/my-react-app/",
   plugins: [react()],
 })
