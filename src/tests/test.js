8fe5218ef035a2058752c1929fe5cd6688dc8d7b
Lekeadeloye <lekeadeloye@yahoo.com>
2023-10-11 15:42:02 +0100

    added mocha and chai

---
+++ src/tests/test.js
@@ -0,0 +1,17 @@
+// import assert from 'assert';
+
+
+// describe('My Test Suite', () => {
+
+//   it ('Check Login', () => {
+//     console.log('Check login passed')
+//   })
+
+//   it ('Check Homepage', () => {
+//     console.log('Check login passed')
+//   })
+
+//   it ('Navigate to homepage', () => {
+//     console.log('Check login passed')
+//   })
+// })
