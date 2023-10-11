6854a211493221e75c4a8e5c39037992c9804235
Lekeadeloye <lekeadeloye@yahoo.com>
2023-10-11 17:29:21 +0100

    updated vite.config.js

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,17 +1,16 @@
-// import assert from 'assert';
+import assert from 'assert';
 
+describe('My Test Suite', () => {
 
-// describe('My Test Suite', () => {
+  it ('Check Login', () => {
+    console.log('Check login passed')
+  })
 
-//   it ('Check Login', () => {
-//     console.log('Check login passed')
-//   })
+  it ('Check Homepage', () => {
+    console.log('Check login passed')
+  })
 
-//   it ('Check Homepage', () => {
-//     console.log('Check login passed')
-//   })
-
-//   it ('Navigate to homepage', () => {
-//     console.log('Check login passed')
-//   })
-// })
+  it ('Navigate to homepage', () => {
+    console.log('Check login passed')
+  })
+})
